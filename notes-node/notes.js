e088632e8fc572e1ca24c5fae9b4169748708d48
anakin13 <bogdangabrielcomsa13@gmail.com>
2017-09-22 00:00:58 +0300

    Added persistence v1

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,11 +1,29 @@
 console.log('Notes file');
 
+const fs = require('fs');
+const _ = require('lodash')
+
 const addNote = (title, body) => {
-  console.log('Adding note', title + ' ' + body)
+  console.log('Adding note', title + ' ' + body);
+
+  const note = {
+    title,
+    body,
+  };
+  const fd = fs.openSync('notes.json', 'w+');
+
+  const noteString = JSON.stringify(note);
+
+  fs.writeFileSync(fd, noteString);
 };
 
 const getAll = () => {
-  console.log('Getting all notes');
+  console.log('Getting all notes \n');
+
+  const notesString = fs.readFileSync('notes.json');
+  const notesObj = JSON.parse(notesString);
+
+  console.log(notesObj);
 };
 
 const getNote = (title) => {
