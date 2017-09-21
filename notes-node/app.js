6b9590c234bd75114aa11710fcc529e18c4f578c
anakin13 <bogdangabrielcomsa13@gmail.com>
2017-09-21 18:39:20 +0300

    Before JSON lecture

---
+++ notes-node/app.js
@@ -0,0 +1,30 @@
+console.log('Starting app');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+const command = argv._[0];
+
+console.log('argv', argv);
+console.log('command', command);
+
+switch (command) {
+  case 'add':
+    notes.addNote(argv.title, argv.body);
+    break;
+  case 'list':
+    notes.getAll();
+    break;
+  case 'read':
+    notes.getNote(argv.title);
+    break;
+  case 'remove':
+    notes.removeNote(argv.title);
+    break;
+  default:
+    break;
+}
